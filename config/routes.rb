9a8751c8e2bc50decd32380f542cd5f63403106f
Juca <juancamilocc09.jc@gmail.com>
2020-05-12 23:18:03 -0500

    registro de pacientes
    iniciar sesion, cerrar sesion, login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :patients
   resources :cita
   #Main page
   root to: "welcome#index"
   get 'welcome' => "welcome#index"
-
-
-
-  get "pacientes/new" => "pacientes#new"
-  get "pacientes" => "pacientes#index"
 end
